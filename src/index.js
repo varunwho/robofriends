2bd3c0ca70be72685ee471d374d2dacfb3f894fb
Easwaran <easwaran2806@gmail.com>
2020-06-18 11:14:56 +0530

    Building Components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Card from './Card';
 import * as serviceWorker from './serviceWorker';
-import Hello from './Hello';
 import 'tachyons';
+import { robots } from './robots';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Hello greeting={'Hello'+' React Ninja'}/>
-  </React.StrictMode>,
+  <div>
+    <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+  </div>,
   document.getElementById('root')
 );
 
